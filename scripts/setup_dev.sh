5ff9b93baec2fc3eed015a0165cfc5d531c31cf7
Jean-Philippe Goulet <jp.goulet17@gmail.com>
2016-03-18 16:21:46 -0400

    Update scripts
    Remove PGDATA from env_cfg, since it's useless for the current user (not
    the postgres user).
    Add a upgrade script for PGSQL to upgrade a DB to a new version.
    Add vim as the VISUAL & EDITOR

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -18,6 +18,7 @@ sudo npm install -g $npm_pkgs
 pg_root='/home/postgres'
 echo "Setup & Start Postgresql service (PGROOT = $pg_root)"
 
+# TODO : Maybe add an `export PGROOT=$pg_root`, so we don't need to change the service file OR change the service file in this script
 sudo useradd -m postgres
 sudo -i -u postgres
 sudo mkdir "$pg_root/data"
@@ -25,6 +26,8 @@ initdb -D "$pg_root/data"
 
 # Enable and start the service
 # Note : If this does not work, may require to modify the file copied with the enable call to change the PG_ROOT location
+# Troubleshooting : Go change the 'Environment=PGROOT=/var/lib/postgres' line manually
+# File : /usr/lib/systemd/system/postgresql.service
 sudo systemctl enable postgresql.service
 sudo systemctl start postgresql.service
 
